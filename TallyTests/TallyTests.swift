921817ebbe97da3cba265bc33af7f84c8ab2d218
BarryNeaves <barry.neaves@gmail.com>
2026-06-15 16:41:00 +0100

    Initial Commit

---
+++ TallyTests/TallyTests.swift
@@ -0,0 +1,19 @@
+//
+//  TallyTests.swift
+//  TallyTests
+//
+//  Created by Barry Neaves on 15/06/2026.
+//
+
+import Testing
+@testable import Tally
+
+struct TallyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
